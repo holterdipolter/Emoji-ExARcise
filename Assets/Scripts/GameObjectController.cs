31b082941198c83c6af3f56c4ac9e2756b0cc32c
holterdipolter <simonwedel@gmx.de>
2017-02-14 00:35:44 +0100

    los geht's

---
+++ Assets/Scripts/GameObjectController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GameObjectController : MonoBehaviour {
+
+	public GameObject TargetTangibleObj;
+	public GameObject TargetRefLU;
+	public GameObject TargetRefRO;
+	public GameObject ImageRefLU;
+	public GameObject ImageRefRO;
+
+	public float offsetX;
+	public float offsetY;
+
+	private Vector3 offsetpos;
+	//private float offsetrot=298;
+	private float offsetrot;
+	private Vector3 pos;
+	private Vector3 posWS;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		offsetpos = transform.position - TargetTangibleObj.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// Kallibrieren:
+		// convert Worldspace to Screenspace
+		Vector3 TargetTangibleObjScreenSpace = Camera.main.WorldToScreenPoint(TargetTangibleObj.transform.position);
+		Vector3 TargetRefLUScreenSpace = Camera.main.WorldToScreenPoint(TargetRefLU.transform.position);
+		Vector3 TargetRefROScreenSpace = Camera.main.WorldToScreenPoint(TargetRefRO.transform.position);
+
+		// Compute (distance TO-refLU) / (distance refRO-refLU), -> pos max = 1
+		pos.x = (TargetTangibleObjScreenSpace.x - TargetRefLUScreenSpace.x) / (TargetRefROScreenSpace.x - TargetRefLUScreenSpace.x);
+		pos.y = (TargetTangibleObjScreenSpace.y - TargetRefLUScreenSpace.y) / (TargetRefROScreenSpace.y - TargetRefLUScreenSpace.y);
+
+		// Convert relative distance to distance in Worldspace
+		posWS.x = (pos.x * (ImageRefRO.transform.position.x - ImageRefLU.transform.position.x)) + ImageRefLU.transform.position.x + offsetX;
+		posWS.y = (pos.y * (ImageRefRO.transform.position.y - ImageRefLU.transform.position.y)) + ImageRefLU.transform.position.y + offsetY;
+
+		//posWS.x = (pos.x * 31.27f) - 15.47f;
+		//posWS.y = (pos.y * 21.5f) - 10.7f;
+
+		transform.position = new Vector3(posWS.x,posWS.y,-0.5f);
+
+		print(" qrLU: "+ TargetRefLUScreenSpace + " qrRO: "+ TargetRefROScreenSpace + " TO: "+ TargetTangibleObjScreenSpace + " pos: "+ pos + " RefDistance: " + (ImageRefRO.transform.position.x - ImageRefLU.transform.position.x) + " Heading: " + (Mathf.Rad2Deg*Mathf.Asin(TargetTangibleObj.transform.forward.x)));
+
+
+		// ROTATION
+
+		transform.eulerAngles = new Vector3 (0,0,(Mathf.Rad2Deg * Mathf.Asin(TargetTangibleObj.transform.forward.x) +90));
+
+		//print ("Heading: " + transform.forward.x * Mathf.Rad2Deg);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Canvas text;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Pick Up"))
+			other.gameObject.SetActive(false);
+			text.gameObject.SetActive(true);
+		
+	}
+}
+
