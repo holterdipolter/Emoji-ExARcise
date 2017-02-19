9ffc056c43a44189eadebde0941045a19430c896
WIN-10-ASUS\Dominik <domser.keller@googlemail.com>
2017-02-19 22:26:15 +0100

    added test audio

---
+++ Assets/Scripts/MusicKeepAlive.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicKeepAlive : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private static MusicKeepAlive instance = null;
+	public static MusicKeepAlive Instance {
+		get { return instance; }
+	}
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
