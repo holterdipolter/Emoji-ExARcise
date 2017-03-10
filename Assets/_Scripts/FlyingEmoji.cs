453a935b8d129b31e5d79872ddfcd4bc4274370d
WIN-10-ASUS\Dominik <domser.keller@googlemail.com>
2017-03-10 22:08:40 +0100

    Flying Emojys finale

--- Assets/_Scripts/FlyingEmoji.cs
+++ Assets/_Scripts/FlyingEmoji.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 public class FlyingEmoji : MonoBehaviour {
 	private Vector3 pos;
 	private int zaehler;
-
+	public bool upwards;
+	public float speed;
+	public float angle;
+	int richtung;
 	// Use this for initialization
 	void Start () {
+		if (upwards)
+			richtung = 1;
+		else
+			richtung = -1;
 		pos.x = -18f;
 		pos.y = Random.Range (-8.0f, 8.0f);
+
 
 	}
 
 	void bewegung(){
 		
+		Debug.Log ("angle ist " + angle);
 		pos = transform.localPosition;
 		if (zaehler < 550) {
-			pos.x = pos.x+0.2f;
+			pos.x = pos.x+speed*0.02f;
+			pos.y = pos.y + richtung*angle*0.01f;
 		}
 		}
 		
@@ -26,10 +36,11 @@ public class FlyingEmoji : MonoBehaviour {
 		bewegung ();
 		zaehler++;
 
-		if (pos.x > 18f) {
+		if (pos.x > 18f||pos.y>10||pos.y<-10) {
 			pos.x = -18f;
 			pos.y = Random.Range (-8.0f, 8.0f);
 			zaehler = 0;
+			angle = Random.Range (0.2f, 5f);
 		}
 		transform.localPosition = pos;
 	}
