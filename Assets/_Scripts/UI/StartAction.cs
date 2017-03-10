e6c80aa486cd3ecb11665377420e75f42cc5d78d
WIN-10-ASUS\Dominik <domser.keller@googlemail.com>
2017-03-11 00:05:56 +0100

    Vorbereitung für Level-Scene-Aufrufe

--- Assets/_Scripts/UI/StartAction.cs
+++ Assets/_Scripts/UI/StartAction.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DigitalRuby.SoundManagerNamespace;
 
 
 public class StartAction : MonoBehaviour {
 
 	private int lastLevel;
 	private int currentLevel;
-
+	public AudioSoundManager soundmanager;
 	public void LoadMenu()  {
 		SceneManager.LoadScene("Menu");
 	}
@@ -22,6 +23,7 @@ public class StartAction : MonoBehaviour {
 
 		if (lastLevel == 0) {
 			SceneManager.LoadScene ("Instructions");
+			soundmanager.PlayMusic (2);
 		} else {
 			SceneManager.LoadScene ("LevelMenu");
 		}
@@ -64,6 +66,46 @@ public class StartAction : MonoBehaviour {
 
 	public void LoadLevel5()  {
 		SceneManager.LoadScene(5);
+	}
+
+	public void LoadLevel6()  {
+		SceneManager.LoadScene(6);
+	}
+
+	public void LoadLevel7()  {
+		SceneManager.LoadScene(7);
+	}
+
+	public void LoadLevel8()  {
+		SceneManager.LoadScene(8);
+	}
+
+	public void LoadLevel9()  {
+		SceneManager.LoadScene(9);
+	}
+
+	public void LoadLevel10()  {
+		SceneManager.LoadScene(10);
+	}
+
+	public void LoadLevel11()  {
+		SceneManager.LoadScene(11);
+	}
+
+	public void LoadLevel12()  {
+		SceneManager.LoadScene(12);
+	}
+
+	public void LoadLevel13()  {
+		SceneManager.LoadScene(13);
+	}
+
+	public void LoadLevel14()  {
+		SceneManager.LoadScene(14);
+	}
+
+	public void LoadLevel15()  {
+		SceneManager.LoadScene(15);
 	}
 
 	public void LoadInstructions()  {
